f9f4421e5605845532fd754d112390b49acbab99
RaghavBatchu <batchuraghavendra07@gmail.com>
2026-01-23 22:58:32 +0530

    Implement Rate Limitation

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import router from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(securityMiddleware);
 
 app.use(
   morgan('combined', {
---
+++ src/config/arcjet.js
@@ -0,0 +1,30 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import { isSpoofedBot } from "@arcjet/inspect";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    slidingWindow({
+        mode: "LIVE", // Enforce the rate limit. Use "DRY_RUN" to log only
+        interval: '2s',
+        max:5
+      }),
+  ],
+});
+
+export default aj;
